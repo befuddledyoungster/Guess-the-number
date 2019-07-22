491ef46c2b3bff869681f99300adfb7a4c35e550
befuddledyoungster <44422650+befuddledyoungster@users.noreply.github.com>
2019-07-22 18:38:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 //GUESS THE NUMBER- A random fun game
-
 #include<iostream>
 #include <bits/stdc++.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
+    //Initiate seed
+    srand(time(0));
     // Takes input as the number of digits of number user wants
     int digits;
     cout << "Enter the no. of digits of the number you wanna guess."<<endl;
@@ -15,14 +17,14 @@ int main()
     //This is the range between which the random no. is generated.In pyhton its very easy to do while in C and Cpp you need to do this.
     int ans1 = pow(10 , digits) -1;
     int ans2 = pow(10,digits -1);
-    int ans = ans2 + rand()% (ans1 -ans2 +1);
+    int ans = ans2 + srand()% (ans1 -ans2 +1);
 
     int temp = 0;
     while(pow(2, temp)< pow(10,digits))
     {
       temp ++;
     }
-    int cnt = temp;
+    int cnt = temp - 1 ;
 
     // Tells you about the tries left and whether you are close to your target or not
     while (cnt --)
